456e79a52e701f8a8e5fbdc2de49aba9ff4c5b80
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-16 06:40:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,7 @@ def batch_badge_creator(people)
 end
 
 def assign_rooms(people)
-  
+  people.map.with_index(1) do |person, index|
+    "Hello, #{person}!  You'll be assigned to room #{index}!"
+  end
+end
